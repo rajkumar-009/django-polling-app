7ca036c9e5981e0fd4b73c963a4e99f6f2e3de1a
rsures16 <rsures16@asu.edu>
2021-06-09 12:58:30 -0700

    Full functionality developed and tested

--- pollingApp/polls/admin.py
+++ pollingApp/polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Question, Choice
 # Register your models here.
+
+admin.site.site_header = "Polling Admin"
+admin.site.site_title = "Polling Admin Area"
+admin.site.index_title = "Welcome to the Polling admin area"
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}), ]
+    inlines = [ChoiceInLine]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
